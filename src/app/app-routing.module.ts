975d84ec6af238af6ef20266bfb9f514282672c5
Calebe-Dev <calebe.araujo101@gmail.com>
2024-12-01 19:25:53 -0300

    Definindo rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeAgendaComponent } from './home-agenda/home-agenda.component';
+import { TableCompromissosComponent } from './table-compromissos/table-compromissos.component';
+import { FormCompromissosComponent } from './form-compromissos/form-compromissos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'/home', pathMatch:'full'},
+  { path: 'home', component: HomeAgendaComponent},
+  { path: 'compromissos', component: TableCompromissosComponent},
+  { path: 'compromissos/:id', component: FormCompromissosComponent},
+  { path: 'compromisso', component: FormCompromissosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/compromissos.ts
@@ -0,0 +1,10 @@
+export interface compromissos {
+    id:number;
+    title:string;
+    description:string;
+    compromissoDate:string;
+    startTime:string;
+    endTime:string;
+    local:string;
+    priority:number; 
+  }
