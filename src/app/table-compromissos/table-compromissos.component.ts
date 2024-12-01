dc49f2768baef729489075f8b8162ab8b24e7e7f
Calebe-Dev <calebe.araujo101@gmail.com>
2024-12-01 19:50:09 -0300

    Adicionando tabela

--- src/app/table-compromissos/table-compromissos.component.ts
+++ src/app/table-compromissos/table-compromissos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { compromissos } from '../compromissos';
+import { AgendaService } from '../agenda.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table-compromissos',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './table-compromissos.component.css'
 })
 export class TableCompromissosComponent {
+
+  Compromissos: compromissos [] = [];
+
+    constructor(private service: AgendaService,
+    private router: Router
+    ){}
+
+    ngOnInit(){
+      this.loadCompromissos();
+    }
+
+    loadCompromissos(){
+      this.service.getCompromissos().subscribe({
+        next: data => this.Compromissos = data
+      })
+    };
+
+    delete(Compromissos: compromissos){
+      this.service.delete(Compromissos).subscribe({
+        next: () => this.loadCompromissos()
+      })
+    };
+
+    create(){
+      this.router.navigate(['compromisso'])
+    };
+
 
 }
